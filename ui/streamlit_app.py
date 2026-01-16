4923bf79c4981640a8e10ce8b50962630c8f598c
BeingAnurag <lci2023036@iiitl.ac.in>
2026-01-16 23:25:01 +0530

    chore: Change backend URL port

--- ui/streamlit_app.py
+++ ui/streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-BACKEND_URL = "http://127.0.0.1:8000/evaluate"
+BACKEND_URL = "http://127.0.0.1:8001/evaluate"
 
 st.set_page_config(page_title="TrustNet AI", layout="centered")
 
